3549c95e49cd5185489644b12288c9ebe9b9ffe0
mic mic <michel.michael38@gmail.com>
2021-12-23 20:02:28 +0100

    Ajout des touches haut bas gauche droite dans un enum et changement de photos lorsque l'on appuie sur une des touche

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
 #include <SDL.h>
-#include <cstdlib>
+#include <cstdio>
+#include <string>
 //LES SOURCES : http://lazyfoo.net/tutorials/SDL/01_hello_SDL/index2.php
 
 //largeur et hauteur de la fenètre
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+
+//Liste des touches dans uen enumation (a la place de int blabla = 0)
+enum KeyPressSurfaces{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL,
+};
 
 //3fonction
 //Demarre SDL + creer la fenètre
@@ -15,6 +25,9 @@ bool loadMedia();
 //Ferme App
 void close();
 
+//Charger les images individuellement
+SDL_Surface* loadSurface(std::string path);
+
 //Initialisation des reference aux variables (pointeur*) + &var
 //Ces variables ont une portée globale
 //Init du rendu
@@ -22,12 +35,13 @@ SDL_Window* gWindow = NULL;
 //La surface contenue dans la fenètre
 //SDL surface est une donnée image qui contient des pixels il use le Processeur GPU
 SDL_Surface* gScreenSurface = NULL;
-//Une image chargée dans la fenètre
-SDL_Surface* gImage = NULL;
-//Quiiter l'application
-bool quit = false;
-//Ajouter des evenements (key_press + mouse_motion + joy_button_press etc...)
-SDL_Event event;
+
+//Une image lors de touche préssée
+SDL_Surface* gKeyPressedSurfaces[KEY_PRESS_SURFACE_TOTAL];
+
+//Image courante affichée
+SDL_Surface* gCurrentSurface = NULL;
+
 
 //Les pointeur alloue la memoire (pour utiliser la meme image au lieu de l'appeler plusieur fois)
 
@@ -65,29 +79,82 @@ bool loadMedia(){
     //de nouveau une condition de succès
     bool success = true;
 
+    //Charger la surface & image par defaut
+    gKeyPressedSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("images/splash2.bmp");
+    if(gKeyPressedSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL){
+        printf("Erreur de chargement de la photo");
+        success = false;
+    }
+
+    //Charger image UP
+    gKeyPressedSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("images/up.bmp");
+    if(gKeyPressedSurfaces[KEY_PRESS_SURFACE_UP] == NULL){
+        printf("Erreur de chargement de la photo");
+        success = false;
+    }
+
+    //Image down
+    gKeyPressedSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("images/down.bmp");
+    if(gKeyPressedSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL){
+        printf("Erreur de chargement de la photo");
+        success = false;
+    }
+
+    //Image left
+    gKeyPressedSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("images/left.bmp");
+    if(gKeyPressedSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL){
+        printf("Erreur de chargement de la photo");
+        success = false;
+    }
+
+    //Image right
+    gKeyPressedSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("images/right.bmp");
+    if(gKeyPressedSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL){
+        printf("Erreur de chargement de la photo");
+        success = false;
+    }
+
     //Chargement de l'image d'accueil = LoadBMP = chemin.bmp
+    /*
     gImage = SDL_LoadBMP("images/splash.bmp");
     if(gImage == NULL){
         printf("Impossible de charger l'image %s\n", "images/splash.bmp", SDL_GetError());
         success = false;
     }
+     */
     return success;
 }
 
 //Fonction fermer la fenètre
 //Libere les medias et arrete SDL
 void close(){
-    //Dealloué la surface de l'image
-    SDL_FreeSurface(gImage);
+    //Dealloué la surface = on boucle sur le nombre touche possible dans enumeration
+    for(int i = 0; i < KEY_PRESS_SURFACE_TOTAL; i++){
+        //Dealloué la surface de l'image
+        SDL_FreeSurface(gKeyPressedSurfaces[i]);
+        gKeyPressedSurfaces[i] = NULL;
+    }
+
     //Detruis la fenètre
     SDL_DestroyWindow(gWindow);
     //Quitte le sous syteme SDL
+    gWindow = NULL;
     SDL_Quit();
+}
+
+//Chager les différentes images
+SDL_Surface* loadSurface(std::string path){
+    //Charger une image avec un chemin precis
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+    if(loadedSurface == NULL){
+        printf("Erreur de chargement des photos BMP %s!", path.c_str(), SDL_GetError());
+    }
+    return loadedSurface;
 }
 
 
 
-int WinMain(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     //Appel de la fonction init()
     if(!init()){
         printf("Erreur d'initialisation SDL !");
@@ -99,21 +166,43 @@ int WinMain(int argc, char* argv[]) {
             //On attend que l'utilisateur quitte l'app
             bool quit = false;
 
+            SDL_Event event;
+            //Image par defaut
+            gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
             //Boucle d'attente que app soit quitter
             while (!quit){
                 while (SDL_PollEvent(&event) != 0){
                     //Utilisateur quitte la fenètre
                     if(event.type == SDL_QUIT){
                         quit = true;
+                        //Detecte un evenement une touche présée
+                    } else if(event.type == SDL_KEYDOWN){
+                        //On creer un Switch case et on appel image grace a enumeration
+                        switch (event.key.keysym.sym) {
+                            case SDLK_UP:
+                                gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_UP];
+                                break;
+                            case SDLK_DOWN:
+                                gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_DOWN];
+                                break;
+                            case SDLK_LEFT:
+                                gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_LEFT];
+                                break;
+                            case SDLK_RIGHT:
+                                gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_RIGHT];
+                                break;
+                            default:
+                                gCurrentSurface = gKeyPressedSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+                                break;
+                        }
                     }
                 }
                 //Sinon tous marche (source de image + null + destination image + null)
-                SDL_BlitSurface(gImage, nullptr, gScreenSurface, nullptr);
+                SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
                 //Mise a jour de la fenètre
                 SDL_UpdateWindowSurface(gWindow);
-                SDL_Delay(2000);
             }
-
         }
     }
     //Libere la resources et ferme SDL
